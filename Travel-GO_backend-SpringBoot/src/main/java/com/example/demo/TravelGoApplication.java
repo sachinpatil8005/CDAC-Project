5ec6407149df65c97f3852ca7c3ce294e61fd960
samruddha kumbhar <samruddhakumbhar@gmail.com>
2023-08-30 10:05:46 +0530

    Update TravelGoApplication.java

--- Travel-GO_backend-SpringBoot/src/main/java/com/example/demo/TravelGoApplication.java
+++ Travel-GO_backend-SpringBoot/src/main/java/com/example/demo/TravelGoApplication.java
@@ -1,1 +1,15 @@
- 
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelGoApplication.class, args);
+		
+		System.out.println("Spring STARTED");
+	}
+
+} 
